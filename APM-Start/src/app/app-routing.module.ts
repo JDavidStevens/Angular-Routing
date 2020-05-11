c2653ee7c11b2f48e789d54cf58db55283968bb5
David Stevens <james.stevens@healthcatalyst.com>
2020-05-11 17:51:15 -0600

    course complete

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { RouterModule } from '@angular/router';
+import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([
             {path: 'welcome', component: WelcomeComponent},
+            {
+                path: 'products',
+                canActivate: [AuthGuard],
+                data: { preload: true },
+                loadChildren: () =>
+                    import('./products/product.module').then(m => m.ProductModule)
+            },
             {path: '', redirectTo: 'welcome', pathMatch: 'full'},
             {path: '**', component: PageNotFoundComponent}
-          ]),
+          ], {preloadingStrategy: SelectiveStrategy}),
     ],
     exports: [RouterModule]
 })
--- APM-Start/src/app/user/auth.guard.ts
+++ APM-Start/src/app/user/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, CanLoad, CanDeactivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, Route } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanDeactivate<unknown> {
+export class AuthGuard implements CanActivate, CanDeactivate<unknown>, CanLoad {
 
   constructor(private authService: AuthService, private router: Router) {}
 
@@ -15,6 +15,11 @@ export class AuthGuard implements CanActivate, CanDeactivate<unknown> {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.checkLoggedIn(state.url);
   }
+
+  canLoad(route: Route): boolean {
+    return this.checkLoggedIn(route.path);
+  }
+
   canDeactivate(
     component: unknown,
     currentRoute: ActivatedRouteSnapshot,
